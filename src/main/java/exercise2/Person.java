9cfc65e13dbe608708d936a592e806fce7768c0b
171440 <fotso.rebecca@yahoo.fr>
2019-10-23 15:43:13 +0200

    ok

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,27 @@
 package exercise2;
 
 class Person {
+	private int age;
+	private static int totalAge = 0;
+	private static int populationSize = 0;
+
+	public Person(int age) {
+		this.age = age;
+		totalAge += this.age;
+		populationSize++;
+	}
+
+	static int computePopulationSize() {
+		return Person.computePopulationSize();
+	}
+
+	static float computeAveragePopulationAge()  {
+		return totalAge/populationSize;
+	}
+	static void resetPopulation()
+	{
+		totalAge = 0;
+		populationSize = 0;
+	}
+
 }
