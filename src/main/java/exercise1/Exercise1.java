e945a8b60e5017c39b32d2d60c4220b7e2974c6d
171440 <fotso.rebecca@yahoo.fr>
2019-10-23 15:09:52 +0200

    ok

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -1,16 +1,26 @@
 package exercise1;
 
+
 public class Exercise1 {
-	static Person createPerson(String name, int age) {
+	static Person createPerson(String name, int age) {    // on peut appeler direct
+
 		// Ajoutez les champs name et age à la classe Person.
 		// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
 		// Créez des getters publics pour lire ces valeurs une fois la classe construite.
-		return null;
+		//System.out.println("vous vous apprêtez à construire une personne");
+		Person p = new Person(name, age);
+		return p;
 	}
 
 	public static void main(String[] args) {
 		// Considérant que cette classe est démarrée en ligne de commande avec un premier paramètre donnant le nom
 		// et un second donnant l'âge (nombre entier), créez un objet Person sur base de ceux-ci.
 		// Ecrivez ensuite le nom et l'âge de cette personne sur la sortie standard.
+		String nom = "toto";
+		int age = 20;
+
+		Person p=Exercise1.createPerson(args[0], Integer.parseInt(args[1]));
+		System.out.println(p.getName());
+		System.out.println(p.getAge());
 	}
 }
--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,26 @@
 package exercise1;
 
 class Person {
+		private String name;
+	private int age;
+	public Person(String nom, int monage){  // création du constructeur
+		this.name=nom;  // this.name est le "name" définit ds le private
+		this.age= monage;
+	}
+	public  String getName ()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public  int getAge ()
+	{
+		return age;
+	}
+	public void setAge(String age)
+	{
+		this.name = age;
+	}
 }
